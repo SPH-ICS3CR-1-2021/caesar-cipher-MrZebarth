bedf4452feb69f3faff5e78b84a0303ac451e2db
MrZebarth <kris.zebarth@ocsb.ca>
2020-12-09 13:25:41 -0500

    Initial commit

---
+++ Caesar.py
@@ -0,0 +1,13 @@
+#You have to read in a message as a string
+message = input("Enter a message: ")
+#You have to read in a shift as a number (1-25)
+
+#You have to loop through every letter
+	#if the character is not a letter:
+#		go to the next letter
+	#turn the letter into a number
+	#add the shift to the letter
+	#f the letter is over the edge of the alphabet
+#		subtract 26 from the letter
+	#turn the number back to a letter
+	#print the letter
