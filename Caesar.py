fa37fd578e63b584026726d22bcafc9afc99b64b
Kris Zebarth <kris.zebarth@ocsb.ca>
2020-12-10 12:50:12 -0500

    Updated final project

--- Caesar.py
+++ Caesar.py
@@ -1,13 +1,30 @@
 #You have to read in a message as a string
 message = input("Enter a message: ")
+output=""
 #You have to read in a shift as a number (1-25)
-
+shift = int(input("Enter a shift (1-25)"))%26
 #You have to loop through every letter
+for letter in message:
 	#if the character is not a letter:
-#		go to the next letter
+    if not (letter.isalpha()):
+		#go to the next letter
+        output+=letter
+        continue
+    #Check for Upper/Lower case
+    if letter.islower():
+        lowerCase=True
+    else:
+        lowerCase=False
 	#turn the letter into a number
+    value = ord(letter)
 	#add the shift to the letter
-	#f the letter is over the edge of the alphabet
-#		subtract 26 from the letter
+    value+=shift
+	#if the letter is over the edge of the alphabet
+    if (lowerCase and value>ord('z')) or (not lowerCase and value>ord('Z')):
+		#subtract 26 from the letter
+        value-=26
 	#turn the number back to a letter
-	#print the letter
+    value=chr(value)
+    #print the letter
+    output+=value
+print(output)
